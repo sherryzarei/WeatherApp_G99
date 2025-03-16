78f46f7a7b19c45461e47e99dde0d8b4af2392df
Sherry Zarei <124946038+sherryzarei@users.noreply.github.com>
2025-03-16 14:04:58 -0400

    Added Network Manager

---
+++ WeatherApp_G99/NetworkManager/NetworkManagerProtocol.swift
@@ -0,0 +1,14 @@
+//
+//  ForcastTemperature.swift
+//  WeatherApp_G99
+//
+//  Created by Shaghayegh zarei on 2025-03-14.
+//
+
+import UIKit
+
+protocol NetworkManagerProtocol {
+    func fetchCurrentWeather(city: String, completion: @escaping (WeatherModel) -> ())
+    func fetchCurrentLocationWeather(lat: String, lon: String, completion: @escaping (WeatherModel) -> ())
+    func fetchNextFiveWeatherForecast(city: String, completion: @escaping ([ForecastTemperature]) -> ())
+}
